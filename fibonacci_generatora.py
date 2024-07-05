d1eef789cfa9cb5baae7cf4d2dabc6123114b724
prince6201 <155984611+prince6201@users.noreply.github.com>
2024-07-05 21:47:50 +0530

    Add files via upload
    ok

---
+++ fibonacci_generatora.py
@@ -0,0 +1,8 @@
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a 
+        a, b = b, a + b
+fib_gen = fibonacci_generator()
+for i in range(10):
+    print(next(fib_gen))
